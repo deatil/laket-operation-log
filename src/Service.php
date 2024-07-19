d76200cb88124dcb43601d4539ca2dbe8b83f0ff
deatil <2217957370@qq.com>
2024-07-19 11:30:40 +0800

    更新

--- src/Service.php
+++ src/Service.php
@@ -18,6 +18,11 @@ class Service extends BaseService
      */
     public $composer = __DIR__ . '/../composer.json';
     
+    /**
+     * 包名
+     */
+    protected $pkg = 'laket/laket-operation-log';
+    
     protected $slug = 'laket-admin.flash.operation-log';
     
     /**
@@ -28,6 +33,18 @@ class Service extends BaseService
         Flash::extend('laket/laket-operation-log', __CLASS__);
     }
     
+    /**
+     * 在插件安装、插件卸载等操作时有效
+     */
+    public function action()
+    {
+        register_install_hook($this->pkg, [$this, 'install']);
+        register_uninstall_hook($this->pkg, [$this, 'uninstall']);
+        register_upgrade_hook($this->pkg, [$this, 'upgrade']);
+        register_enable_hook($this->pkg, [$this, 'enable']);
+        register_disable_hook($this->pkg, [$this, 'disable']);
+    }
+
     /**
      * 开始，只有启用后加载
      */
